b15002104b8728a65c1aa2641af82f3cf348df6e
KyleQiao1992 <chocolatman1992@gmail.com>
2023-10-17 11:22:20 -0400

    up

--- src/main/java/com/kaitian/fukingalgrithm/demo/Test.java
+++ src/main/java/com/kaitian/fukingalgrithm/demo/Test.java
@@ -9,7 +9,7 @@ public class Test {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1), p = dummy;
         ListNode p1 = list1, p2 = list2;
-        whlie (p1 != null & p2 != null) {
+        while (p1 != null & p2 != null) {
             if (p1.val > p2.val) {
                 p.next = p2;
                 p2 = p2.next;
