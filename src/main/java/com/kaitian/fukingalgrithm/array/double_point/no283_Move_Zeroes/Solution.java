42db378c5979ed4e6b798894025d2ea2b57b0b74
KyleQiao1992 <chocolatman1992@gmail.com>
2023-12-22 17:56:34 +0800

    up

--- src/main/java/com/kaitian/fukingalgrithm/array/double_point/no283_Move_Zeroes/Solution.java
+++ src/main/java/com/kaitian/fukingalgrithm/array/double_point/no283_Move_Zeroes/Solution.java
@@ -3,25 +3,27 @@ package com.kaitian.fukingalgrithm.array.double_point.no283_Move_Zeroes;
 public class Solution {
 
     public void moveZeroes(int[] nums) {
-        int p = removeElement(nums, 0);
-        for (; p < nums.length; p++) {
-            nums[p] = 0;
+        int slow = removeElement(nums, 0);
+
+        for (int i = slow; i < nums.length; i++) {
+            nums[i] = 0;
         }
     }
 
-    private int removeElement(int[] nums, int val) {
-        int slow = 0, fast = 0;
-
-        while (fast < nums.length) {
-            if (nums[fast] != val) {
+    private int removeElement(int[] nums, int target) {
+        int fast = 0;
+        int slow = 0;
+        while (fast <= nums.length - 1) {
+            if (target == nums[fast]) {
+                fast++;
+            } else {
                 nums[slow] = nums[fast];
                 slow++;
+                fast++;
             }
-            fast++;
         }
         return slow;
     }
-
 
     public static void main(String[] args) {
         Solution f = new Solution();
