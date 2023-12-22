13165f2a46b71f29608d2e012d8892612ce40e12
KyleQiao1992 <chocolatman1992@gmail.com>
2023-12-22 13:35:15 +0800

    up

--- src/main/java/com/kaitian/fukingalgrithm/array/no27_Remove_Element/Solution.java
+++ src/main/java/com/kaitian/fukingalgrithm/array/no27_Remove_Element/Solution.java
@@ -2,11 +2,14 @@ package com.kaitian.fukingalgrithm.array.no27_Remove_Element;
 
 public class Solution {
 
+    //fast:寻找新数组的元素 ，新数组就是不含有目标元素的数组
+    //slow:指向更新 新数组下标的位置
     public int removeElement(int[] nums, int val) {
-        int slow = 0, fast = 0;
+        int fast = 0;
+        int slow = 0;
 
-        while (fast < nums.length) {
-            if (nums[fast] != val) {
+        while (fast <= nums.length - 1) {
+            if (val != nums[fast]) {
                 nums[slow] = nums[fast];
                 slow++;
             }
